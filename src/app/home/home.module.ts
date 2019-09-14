c12cf4c60ce99b5b3df4b7f748e743c99d0b7688
Feira das Profissões 2019 <feira2019@labcomp.usc>
2019-09-14 11:12:39 -0300

    init project

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+import { HomePage } from './home.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage],
+  providers: [
+    Camera
+  ]
+})
+export class HomePageModule {}
